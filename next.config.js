45bbf1f94573c97c5788494c710e8a0d9974a186
MinhajHameed <minhajham@yahoo.com>
2024-01-19 20:03:35 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const dotenv = require('dotenv');
 
-module.exports = nextConfig
+const { parsed } = dotenv.config({ path: '.env' });
+
+module.exports = {
+  env: {
+    RAPID_API_KEY: parsed.RAPID_API_KEY,
+  },
+  // Your other configurations...
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        satoshi: ['satoshi', 'sans-serif'],
+        inter: ['inter', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
